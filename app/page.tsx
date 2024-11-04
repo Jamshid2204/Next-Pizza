fecf6e12a077057b40e40cc9e13a28f35e7f6d86
Jamshid2204 <jamshidjumaboyev2204@gmail.com>
2024-11-04 20:35:43 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import { Container } from "@/components/shared";
+import { Categories, Container } from "@/components/shared";
 import { Title } from "@/components/shared/title";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
         <Title text="Все Пиццы" size="lg" className="font-extrabold"/>
+
+        <Categories/>
       </Container>
     </>
     
