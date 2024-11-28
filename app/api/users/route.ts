837d136f4b11a8b6d244ba1e16cef1b5ef9a27f8
Jamshid2204 <jamshidjumaboyev2204@gmail.com>
2024-11-28 23:45:47 +0500

    day-9

---
+++ app/api/users/route.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/prisma/prisma-client";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+
+    const users = await prisma.user.findMany();
+
+    return NextResponse.json(users);
+}
+
+export async function POST(req: NextResponse) {
+    const data = await req.json();
+
+    const users = await prisma.user.create({
+        data
+    })
+
+    return NextResponse.json(users);
+}
